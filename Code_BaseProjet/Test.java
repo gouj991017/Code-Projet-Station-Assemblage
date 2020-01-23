fcc9ba3a0752a3f365bb2ca95a8db1e36eae7952
gouj991017 <60113080+gouj991017@users.noreply.github.com>
2020-01-23 16:40:07 -0500

    Code de la base V2

--- Code_BaseProjet/Test.java
+++ Code_BaseProjet/Test.java
@@ -24,7 +24,7 @@ import java.sql.*;
 
 public class Test
 {
-    static final int NB_BACS = 4; //Constante du nombre de bacs relie au Raspberry pi.
+    static final int NB_BACS = 20; //Constante du nombre de bacs relie au Raspberry pi.
     // instance variables - replace the example below with your own
     static GpioPinDigitalOutput t_outputIOs[] = new GpioPinDigitalOutput[NB_BACS];
     static GpioPinDigitalInput t_inputIOs[] = new GpioPinDigitalInput[NB_BACS];
@@ -51,102 +51,68 @@ public class Test
         GpioController gpio = GpioFactory.getInstance();
         
         //Provision gpio pin 1,3,5,7 as an output pin
-        t_outputIOs[0] = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "LED1", PinState.LOW);
-        t_outputIOs[1] = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "LED2", PinState.LOW);
-        t_outputIOs[2] = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "LED3", PinState.LOW);
-        t_outputIOs[3] = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "LED4", PinState.LOW);
-        
+        /*
+        t_outputIOs[0] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_00, "Bac1", PinPullResistance.PULL_DOWN);
+        t_outputIOs[1] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_01, "Bac2", PinPullResistance.PULL_DOWN);
+        t_outputIOs[2] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_03, "Bac3", PinPullResistance.PULL_DOWN);
+        t_outputIOs[3] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_04, "Bac4", PinPullResistance.PULL_DOWN);
+        t_outputIOs[4] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_05, "Bac5", PinPullResistance.PULL_DOWN);
+        t_outputIOs[5] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_06, "Bac6", PinPullResistance.PULL_DOWN);
+        t_outputIOs[6] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_07, "Bac7", PinPullResistance.PULL_DOWN);
+        t_outputIOs[7] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_08, "Bac8", PinPullResistance.PULL_DOWN);
+        t_outputIOs[8] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_09, "Bac9", PinPullResistance.PULL_DOWN);
+        t_outputIOs[9] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_10, "Bac10", PinPullResistance.PULL_DOWN);
+        t_outputIOs[10] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_11, "Bac11", PinPullResistance.PULL_DOWN);
+        t_outputIOs[11] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_12, "Bac12", PinPullResistance.PULL_DOWN);
+        t_outputIOs[12] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_13, "Bac13", PinPullResistance.PULL_DOWN);
+        t_outputIOs[13] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_14, "Bac14", PinPullResistance.PULL_DOWN);
+        t_outputIOs[14] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_15, "Bac15", PinPullResistance.PULL_DOWN);
+        t_outputIOs[15] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_16, "Bac16", PinPullResistance.PULL_DOWN);
+        t_outputIOs[16] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_17, "Bac17", PinPullResistance.PULL_DOWN);
+        t_outputIOs[17] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_18, "Bac18", PinPullResistance.PULL_DOWN);
+        t_outputIOs[18] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_19, "Bac19", PinPullResistance.PULL_DOWN);
+        t_outputIOs[19] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_20, "Bac20", PinPullResistance.PULL_DOWN);
+        t_outputIOs[20] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_21, "Bac21", PinPullResistance.PULL_DOWN);
+        t_outputIOs[21] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_22, "Bac22", PinPullResistance.PULL_DOWN);
+        t_outputIOs[22] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_23, "Bac23", PinPullResistance.PULL_DOWN);
+        t_outputIOs[23] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_24, "Bac24", PinPullResistance.PULL_DOWN);
+        t_outputIOs[24] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_25, "Bac25", PinPullResistance.PULL_DOWN);
+        t_outputIOs[25] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_26, "Bac26", PinPullResistance.PULL_DOWN);
+        t_outputIOs[26] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_27, "Bac27", PinPullResistance.PULL_DOWN);
+        t_outputIOs[27] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_28, "Bac28", PinPullResistance.PULL_DOWN);
+        */
         //Provision gpio pin 0,2,4,6 as an input pin with its internal pull down resistor enabled
-        t_inputIOs[0] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_00, "Bac1", PinPullResistance.PULL_UP);
-        t_inputIOs[1] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, "Bac2", PinPullResistance.PULL_UP);
-        t_inputIOs[2] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_04, "Bac3", PinPullResistance.PULL_UP);
-        t_inputIOs[3] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_06, "Bac4", PinPullResistance.PULL_UP);
-        
-        
-        String user = "admin";
-        String password = "admin";
-        String host = "localhost"; // Possiblement a modifier***********************************************************************************************
-        int port = Integer.parseInt("1883");
-        final String destination = "/scal/scal_reponse_requete"; // A modifier*************************************************************************************
-        
-        JSONObject initBaseJsonObj = new JSONObject();
-        initBaseJsonObj.put("Base", "Debut de la communication"); //Note: Trouver comment mettre des accents*****
-        initBaseJsonObj.put("Base", "Suivez attentivement les instructions de votre guide a l'etape indiquee");
-        initBaseJsonObj.put("Base", "Commence avec l'etape numero 1");
-        
-        MQTT mqtt = new MQTT();
-        mqtt.setHost(host, port);
-        mqtt.setUserName(user);
-        mqtt.setPassword(password);
-        
-        BlockingConnection connection = mqtt.blockingConnection();
-        connection.connect();
-        
-        String TOPIC_REPONSE = "/scal/scal_requete_acces";
+        t_inputIOs[0] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_00, "Bac1", PinPullResistance.PULL_DOWN);
+        t_inputIOs[1] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_01, "Bac2", PinPullResistance.PULL_DOWN);
+        t_inputIOs[2] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_03, "Bac3", PinPullResistance.PULL_DOWN);
+        t_inputIOs[3] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_04, "Bac4", PinPullResistance.PULL_DOWN);
+        t_inputIOs[4] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_05, "Bac5", PinPullResistance.PULL_DOWN);
+        t_inputIOs[5] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_06, "Bac6", PinPullResistance.PULL_DOWN);
+        t_inputIOs[6] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_07, "Bac7", PinPullResistance.PULL_DOWN);
+        t_inputIOs[7] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_08, "Bac8", PinPullResistance.PULL_DOWN);
+        t_inputIOs[8] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_09, "Bac9", PinPullResistance.PULL_DOWN);
+        t_inputIOs[9] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_10, "Bac10", PinPullResistance.PULL_DOWN);
+        t_inputIOs[10] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_11, "Bac11", PinPullResistance.PULL_DOWN);
+        t_inputIOs[11] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_12, "Bac12", PinPullResistance.PULL_DOWN);
+        t_inputIOs[12] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_13, "Bac13", PinPullResistance.PULL_DOWN);
+        t_inputIOs[13] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_14, "Bac14", PinPullResistance.PULL_DOWN);
+        t_inputIOs[14] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_15, "Bac15", PinPullResistance.PULL_DOWN);
+        t_inputIOs[15] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_16, "Bac16", PinPullResistance.PULL_DOWN);
+        t_inputIOs[16] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_17, "Bac17", PinPullResistance.PULL_DOWN);
+        t_inputIOs[17] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_18, "Bac18", PinPullResistance.PULL_DOWN);
+        t_inputIOs[18] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_19, "Bac19", PinPullResistance.PULL_DOWN);
+        t_inputIOs[19] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_20, "Bac20", PinPullResistance.PULL_DOWN);
+        t_inputIOs[20] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_21, "Bac21", PinPullResistance.PULL_DOWN);
+        t_inputIOs[21] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_22, "Bac22", PinPullResistance.PULL_DOWN);
+        t_inputIOs[22] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_23, "Bac23", PinPullResistance.PULL_DOWN);
+        t_inputIOs[23] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_24, "Bac24", PinPullResistance.PULL_DOWN);
+        t_inputIOs[24] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_25, "Bac25", PinPullResistance.PULL_DOWN);
+        t_inputIOs[25] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_26, "Bac26", PinPullResistance.PULL_DOWN);
+        t_inputIOs[26] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_27, "Bac27", PinPullResistance.PULL_DOWN);
+        t_inputIOs[27] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_28, "Bac28", PinPullResistance.PULL_DOWN);
         
         /* *********************Publishing du message d'initialisation******************* */
-        String DATA = initBaseJsonObj.toString(3);
-        Buffer msg = new AsciiBuffer(DATA);
+        for(int i=0;)
         
-        UTF8Buffer topic = new UTF8Buffer(destination);
-        connection.publish(topic, msg, QoS.AT_LEAST_ONCE, false);
-        
-        /* **********************Preparation de la publication du message d'erreur de selection des bacs********************** */
-        JSONObject erreurBaseJsonObj = new JSONObject();
-        erreurBaseJsonObj.put("Base", "Ce n'est pas le bon bac!!!");
-        
-        while(true)
-        {
-            try
-            {
-                t_outputIOs[numPageCourante-1].high();
-                
-                if(t_inputIOs[0].isHigh())
-                {
-                    if(numPageCourante != 1)
-                    {
-                        erreurBaseJsonObj.put("Base", "Allez a l'etape numero " + numPageCourante);
-                        DATA = erreurBaseJsonObj.toString(2);
-                        Buffer msgErreur = new AsciiBuffer(DATA);
-                        connection.publish(topic, msgErreur, QoS.AT_LEAST_ONCE, false);
-                    }
-                }
-                else if(t_inputIOs[1].isHigh())
-                {
-                    if(numPageCourante != 2)
-                    {
-                        erreurBaseJsonObj.put("Base", "Allez a l'etape numero " + numPageCourante);
-                        DATA = erreurBaseJsonObj.toString(2);
-                        Buffer msgErreur = new AsciiBuffer(DATA);
-                        connection.publish(topic, msgErreur, QoS.AT_LEAST_ONCE, false);
-                    }
-                }
-                else if(t_inputIOs[2].isHigh())
-                {
-                    if(numPageCourante != 3)
-                    {
-                        erreurBaseJsonObj.put("Base", "Allez a l'etape numero " + numPageCourante);
-                        DATA = erreurBaseJsonObj.toString(2);
-                        Buffer msgErreur = new AsciiBuffer(DATA);
-                        connection.publish(topic, msgErreur, QoS.AT_LEAST_ONCE, false);
-                    }
-                }
-                else if(t_inputIOs[3].isHigh())
-                {
-                    if(numPageCourante != 4)
-                    {
-                        erreurBaseJsonObj.put("Base", "Allez a l'etape numero " + numPageCourante);
-                        DATA = erreurBaseJsonObj.toString(2);
-                        Buffer msgErreur = new AsciiBuffer(DATA);
-                        connection.publish(topic, msgErreur, QoS.AT_LEAST_ONCE, false);
-                    }
-                }
-            }
-            catch(Exception e)
-            {
-                
-            }
-        }
-                
     }
 }
